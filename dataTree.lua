953ff0e6f7b07d4e9ef5f1320773579f097966f0
Armando Arce <armando.arce@gmail.com>
2018-10-30 11:55:46 -0600

    New aproach using firtChild-nextSibling

--- dataTree.lua
+++ dataTree.lua
@@ -1,33 +1,86 @@
+local DataTree = {}
 
-
-function nodeNew(label,fields)
-  local node = {members=0,height=0,count=0,name=label,leaf=true}
-  if fields then
-    for k,v in pairs(fields) do
-      node[k]=v
+DataTree = function()
+  local table = {_name={},_parent={},_first={},_next={},_last={}}
+  local _table = table
+  table = {}
+  table.nodeNew = function(label,fields)
+   local n = #_table._name + 1
+    _table._name[n] = label
+    _table._parent[n] = 0
+    _table._first[n] = 0
+    _table._next[n] = 0
+    _table._last[n] = 0
+    if fields then
+      for k,v in pairs(fields) do
+        _table[k][n]=v
+      end
+    end
+    return n
+  end
+  table.addChild = function(ndx,label,fields)
+   local n = table.nodeNew(label,fields)
+    _table._parent[n] = ndx
+    if _table._first[ndx] == 0 then
+      _table._first[ndx] = n
+    else
+      _table._next[_table._last[ndx]] = n
+    end
+    _table._last[ndx] = n
+    return n
+  end
+  table.name = function(ndx)
+    return _table._name[ndx]
+  end
+  table.parent = function(ndx)
+    return _table._parent[ndx]
+  end
+  table.firstChild = function(ndx)
+    return _table._first[ndx]
+  end
+  table.nextSibling = function(ndx)
+    return _table._next[ndx]
+  end
+  table.lastChild = function(ndx)
+    return _table._last[ndx]
+  end
+  table.name = function(ndx)
+    return _table._name[ndx]
+  end
+  table.field = function(ndx,key,value)
+    if value then
+      _table[key][ndx] = value
+    else
+      return _table[key][ndx]
     end
   end
-  return node
+  local metatable = {
+    __index = function(table,key)
+      return _table[key]
+    end,
+    __newindex = function(table,key,value)
+      _table[key[1]] = value
+    end
+  }
+  setmetatable(table,metatable)
+  return table
 end
 
-function addChild(node,name,fields)
-  local n = #node + 1
-  local child = nodeNew(name,fields)
-  node[n] = child
-  node[name]= child
-  node.leaf=false
-  node.count = node.count + 1
-  return child
+local tree = DataTree()
+
+function addChild(ndx,label)
+  return tree.addChild(ndx,label)
 end
 
-function printN(node,fields)
+function nodeNew(label)
+  return tree.nodeNew(label)
 end
 
-function asNode(dataFrame)
-end
-
-function clone(node)
-end
-
-function merge(node1,node2)
+function printNode(node,fields)
+  print(tree.name(node))
+  local child = tree.firstChild(node)
+  while child ~= 0 do
+    printNode(child,fields)
+    child = tree.nextSibling(child)
+  end
 end
--- test.lua
+++ test.lua
@@ -13,42 +13,13 @@ acme = nodeNew("Acme Inc.")
     outsource = addChild(it,"Outsource")
     agile = addChild(it,"Go agile")
     goToR = addChild(it,"Switch to R")
-
-printN(acme)
-
--- Navigation by path
-
-print(acme.IT.Outsource.name)
-
-print(acme.Research['New Labs'].name)
-
--- Naviation by position
-
-print(acme[1][2].name)
-
--- Custom fields
-
-acme.Accounting['New Software'].cost = 1000000
-acme.Accounting["New Accounting Standards"].cost = 500000
-acme.Research["New Product Line"].cost = 2000000
-acme.Research["New Labs"].cost = 750000
-acme.IT.Outsource.cost = 400000
-acme.IT["Go agile"].cost = 250000
-acme.IT["Switch to R"].cost = 50000
-
-acme.Accounting["New Software"].p = 0.5
-acme.Accounting["New Accounting Standards"].p = 0.75
-acme.Research["New Product Line"].p = 0.25
-acme.Research["New Labs"].p = 0.9
-acme.IT.Outsource.p = 0.2
-acme.IT["Go agile"].p = 0.05
-acme.IT["Switch to R"].p = 1
-
-printN(acme,{"cost","p"})
+    
+printNode(acme)
 
 -- Custom fields in constructor
 
 birds = nodeNew("Aves", {vulgo = "Bird"})
 addChild(birds,"Neognathae", {vulgo = "New Jaws", species = 10000})
 addChild(birds,"Palaeognathae", {vulgo = "Old Jaws", species = 60})
-printN(birds, "vulgo", "species")
+
+printNode(birds, {"vulgo", "species"})
