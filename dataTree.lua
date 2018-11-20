52ef066828d32afe57eded95ed0df7a0a0228aed
Armando Arce <armando.arce@gmail.com>
2018-11-19 21:16:16 -0600

    Structure changes

--- dataTree.lua
+++ dataTree.lua
@@ -1,11 +1,12 @@
 local write = io.write
+require "dataVector"
+require "CSV"
 
-local DataTree = {}
-
-DataTree = function()
-  local table = {name={},parent={},first={},next={},last={}}
+function dataTree()
+  local table = {name=vector(),parent=vector(),
+    first=vector(),next=vector(),last=vector()}
   table.nodeNew = function(label,fields)
-    local n = #table.name + 1
+    local n = table.name.size() + 1
     table.name[n] = label
     table.parent[n] = 0
     table.first[n] = 0
@@ -35,18 +36,8 @@ DataTree = function()
   return table
 end
 
-local tree = DataTree()
-
-function addChild(ndx,label,fields)
-  return tree.addChild(ndx,label,fields)
-end
-
-function nodeNew(label,fields)
-  return tree.nodeNew(label,fields)
-end
-
-function printNode(node,fields)
-  write(tree.name[node]..' ')
+function printDataNode(tree,node,fields)
+  write(tostring(tree.name[node])..' ')
   if fields then
     for i=1,#fields do
       if tree[fields[i]][node] then
@@ -57,7 +48,11 @@ function printNode(node,fields)
   write('\n')
   local child = tree.first[node]
   while child ~= 0 do
-    printNode(child,fields)
+    printDataNode(tree,child,fields)
     child = tree.next[child]
   end
+end
+
+function printDataTree(tree,fields)
+  printDataNode(tree,1,fields)
 end
--- test.lua
+++ test.lua
@@ -2,24 +2,28 @@ require "dataTree"
 
 -- Create a tree programmatically
 
-acme = nodeNew("Acme Inc.")
-  accounting = addChild(acme,"Accounting")
-    software = addChild(accounting,"New Software")
-    standards = addChild(accounting,"New Accounting Standards")
-  research = addChild(acme,"Research")
-    newProductLine = addChild(research,"New Product Line")
-    newLabs = addChild(research,"New Labs")
-  it = addChild(acme,"IT")
-    outsource = addChild(it,"Outsource")
-    agile = addChild(it,"Go agile")
-    goToR = addChild(it,"Switch to R")
+local tree1 = dataTree()
+acme = tree1.nodeNew("Acme Inc.")
+  accounting = tree1.addChild(acme,"Accounting")
+    software = tree1.addChild(accounting,"New Software")
+    standards = tree1.addChild(accounting,"New Accounting Standards")
+  research = tree1.addChild(acme,"Research")
+    newProductLine = tree1.addChild(research,"New Product Line")
+    newLabs = tree1.addChild(research,"New Labs")
+  it = tree1.addChild(acme,"IT")
+    outsource = tree1.addChild(it,"Outsource")
+    agile = tree1.addChild(it,"Go agile")
+    goToR = tree1.addChild(it,"Switch to R")
     
-printNode(acme)
+printDataTree(tree1)
+
+print()
 
 -- Custom fields in constructor
 
-birds = nodeNew("Aves", {vulgo = "Bird"})
-addChild(birds,"Neognathae", {vulgo = "New Jaws", species = 10000})
-addChild(birds,"Palaeognathae", {vulgo = "Old Jaws", species = 60})
+local tree2 = dataTree()
+birds = tree2.nodeNew("Aves", {vulgo = "Bird"})
+tree2.addChild(birds,"Neognathae", {vulgo = "New Jaws", species = 10000})
+tree2.addChild(birds,"Palaeognathae", {vulgo = "Old Jaws", species = 60})
 
-printNode(birds, {'vulgo', 'species'})
+printDataTree(tree2, {'vulgo', 'species'})
